a1b88d511963451f3b6c6a3c135da6d2c7f5d497
Asif-newaz <asif.newaz0093@gmail.com>
2024-06-12 04:11:11 +0600

    feat: make the layout of the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em></em>
+    </footer>
+  );
+}
+
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+      <select value="form">
+        <option value="car" key="1">
+          car
+        </option>
+      </select>
+      <input type="text" placeholder="Item..." value="form input" />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul></ul>
+      <div className="actions">
+        <select value="items">
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button>Clear list</button>
+      </div>
+    </div>
+  );
+}
---
+++ src/Item.js
@@ -0,0 +1,9 @@
+export default function Item({ item }) {
+  return (
+    <li>
+      <input type="checkbox" value="item" />
+      <span></span>
+      <button>❌</button>
+    </li>
+  );
+}
